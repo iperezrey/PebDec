76716355c3cef0f0ff5320c678cea83469310347
xrigueira <xrigueiradiaz@gmail.com>
2023-01-22 20:47:33 +0100

    Added program to rotate (not tested)

---
+++ rotate.py
@@ -0,0 +1,22 @@
+import os
+# import cv2
+
+# Get the names of all images in the directory
+image_names = os.listdir('images_original')
+
+for image_name in image_names:
+    print(image_name)
+
+    # Read image
+    img = cv2.imread(f'images_original/{image_name}')
+
+    # Get image size
+    h, w = img.shape()
+
+    if (h == 1536) and (w == 2048):
+
+        # Rotate the image 90 degrees clockwise
+        img.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
+        # Save the image
+        img.imwrite(f'images_original/{image_name}', img)
